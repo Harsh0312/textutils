efd0509dd405abdf99038a06008794a0ca241434
Harsh0312 <68933179+Harsh0312@users.noreply.github.com>
2022-10-25 01:34:45 +0530

    issues resolved

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -28,14 +28,14 @@ export default function TextForm(props) {
                 <div className="mb-3 my-3" >
                     <textarea className="form-control" id="exampleFormControlTextarea1" style={{backgroundcolor: props.mode==='light'? 'grey': 'white'}}  onChange={handleOnChange} value={text} rows="8"></textarea>
                 </div>
-                <button className="btn btn-primary mx-3" onClick={handleUpCLick}>Convert to Uppercase</button>
-                <button className="btn btn-primary mx-3" onClick={handleLowClick}>Convert to Lowercase</button>
-                <button className="btn btn-primary mx-3" onClick={handleCLearClick}>Clear</button>
-                <button className="btn btn-primary mx-3" onClick={speak}>SPEAK</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-3 my-1" onClick={handleUpCLick}>Convert to Uppercase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-3 my-1" onClick={handleLowClick}>Convert to Lowercase</button>
+                <button disabled={text.length===0}className="btn btn-primary mx-3 my-1" onClick={handleCLearClick}>Clear</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-3 my-1" onClick={speak}>SPEAK</button>
             </div>
             <div className="container my-3" >
                 <h1>Your text Summary</h1>
-                <p>{(text.split(" ").length-1)} and  {text.length} characters</p>
+                <p>{(text.split(" ").filter((element)=>{return element.length!=0}).length)} words and {text.length} characters</p>
                 <p>{(0.008 * text.split(" ").length)} minutes will be required to read above text</p>
                 <h2>Preview : </h2>
                 <p>{text}</p>
